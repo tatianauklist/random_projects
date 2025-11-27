eb6dabba358b4b09e60ce2cd29f0f90f0b215dbf
Tatiana <tatiana.uklist@gmail.com>
2025-11-27 14:15:22 -0600

    adding in reader too

--- reader.py
+++ reader.py
@@ -1,28 +1,48 @@
 # reading the text
 import re
 
-#names = [r'Bob',r'Linda',r'Gail',r'Teddy',r'Mort',r'Gene',r'Louis',r'Tina']
-names = {}
-while True:
-    name = input("Enter a character name or type 'done' to exit: ")
-    if name == 'done':
-        break
-    names[name] = []
+exclusion = []
+o = open("exclusions.txt","r")
+#content = o.read()
+#print(content)
 
-f = open("Test_Scene.txt")
+for line in o.readlines():
+    word= line.strip()
+    exclusion.append(word)
+print(exclusion)    
+o.close()
+
+#names = {}
+#while True:
+#    name = input("Enter a character name or type 'done' to exit: ")
+#    if name == 'done':
+#        break
+#    names[name] = []
+
+f = open("test_compliance.txt")
 text = f.readlines()
 
-lines = {}
-for name in names:
-    lines[name] = []
+#lines = {}
+#for word in exclusion:
+#   lines[exclusion] = []
 
 for line in text:
-    for x in names:
+    for x in exclusion:
         if re.search(x,line):
             lines[x].append(line)
+for x in lines:
+    print(f"{x} is mentioned {len(lines[x])} times")
 
-for name in lines:
-    print(f"{name}: {len(lines[name])}\n {lines[name]}")
-
+#for x in lines:
+#    for word1,word2 in exclusion:
+#        all_lines = " ".join(lines[x])
+#        if word1 in all_lines and word2 in all_lines:
+#            print(f"{x} is an excluded term")
+#            for line in lines[x]:
+#                if word1 in line:
+#                    print(line)
+#                if word2 in line:
+#                    print(line)
+        
 
 f.close()
