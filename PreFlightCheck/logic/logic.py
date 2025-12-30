52a4d2623453cc84fa8666a905339bc0c096ffd5
Tatiana <tatiana.uklist@gmail.com>
2025-12-29 20:00:38 -0600

    refactoring for different interfaces

--- PreFlightCheck/logic/logic.py
+++ PreFlightCheck/logic/logic.py
@@ -15,6 +15,78 @@ def saveChecklist(data, filePath):
     with open(filePath, 'w') as f:
         yaml.dump(data,f, sort_keys=False)
 
+def generateReport(data):
+    project = data["projectName"]
+    header = f"Pre-Flight Check Results for {project}"
+    divider = "-" * len(header)
+    owner = data["owner"]["name"]
+    yield divider
+    yield header.center(len(header))
+    yield divider
+    blockers = []
+    itemNumberMenu = {}
+    itemNumber = 1
+    for name, categoryData in data.items():
+        if name not in ["projectName", "owner"]:
+            totalChecks = len(categoryData)
+            completedChecks = sum(categoryData.values())
+            percentageCompleted = round((completedChecks / totalChecks) * 100, 2)
+            yield f"{name}: {percentageCompleted}%"
+            for checkName in categoryData:
+                checkValue = categoryData[checkName]
+                if checkValue:
+                    checkValueAnimated = "✅"
+                else:
+                    checkValueAnimated = "❌"
+                yield(f" {itemNumber} {checkName.capitalize()}: {checkValueAnimated}")
+                itemNumberMenu[itemNumber] = {"check": checkName, "value": checkValue, "category": name}
+                itemNumber += 1
+                if not checkValue:
+                    blockers.append({'check': checkName, 'category': name})
+    yield divider
+    header1 = f"Blockers"
+    yield header1.center(len(header))
+    yield divider
+    if len(blockers) <= 0:
+        yield "No blockers found 🥳"
+    else:
+        for blocker in blockers:
+            yield f"{blocker['check']} ({blocker['category']})"
+    yield divider
+
+def getBlockers(data):
+    blockers = []
+    for name, categoryData in data.items():
+        if name not in ["projectName", "owner"]:
+            for checkName in categoryData:
+                checkValue = categoryData[checkName]
+                if not checkValue:
+                    blockers.append({'check': checkName, 'category': name})
+    return blockers
+
+def getNumberedChecks(data):
+    itemNumberMenu = {}
+    itemNumber = 1
+    for name, categoryData in data.items():
+        if name not in ["projectName", "owner"]:
+            for checkName in categoryData:
+                checkValue = categoryData[checkName]
+                itemNumberMenu[itemNumber] = {"check": checkName,"checkValue": checkValue, "category": name}
+                itemNumber += 1
+    return itemNumberMenu
+
+def updateChecklist(data,numberedChecks,updateNumbers):
+    project = data["projectName"]
+    for item in updateNumbers:
+        if item in numberedChecks:
+            menuItem = numberedChecks[item]
+            currentValue = menuItem["checkValue"]
+            newValue = not menuItem["checkValue"]
+            category = menuItem["category"]
+            checkName = menuItem["check"]
+            data[category][checkName] = newValue
+
+    return data
 
 
 
--- PreFlightCheck/logic/main.py
+++ PreFlightCheck/logic/main.py
@@ -1,83 +1,22 @@
-from logic import loadChecklist, saveChecklist
+from logic import loadChecklist, saveChecklist, generateReport, getNumberedChecks, updateChecklist
 import logging
 
 fileType = input("Are you checking software or event readiness?: \n")
 data, filePath = loadChecklist(fileType)
-project = data["projectName"]
-header = f"Pre-Flight Check Results for {project}"
-divider = "-" * len(header)
-owner = data["owner"]["name"]
-print(divider)
-print(header.center(len(header)))
-print(divider)
-blockers = []
-itemNumberMenu = {}
-itemNumber = 1
-for name, categoryData in data.items():
-    if name not in ["projectName", "owner"]:
-        totalChecks = len(categoryData)
-        completedChecks = sum(categoryData.values())
-        percentageCompleted = round((completedChecks/totalChecks) * 100,2)
-        print(f"{name}: {percentageCompleted}%")
-        for checkName in categoryData:
-            checkValue = categoryData[checkName]
-            if checkValue:
-                checkValueAnimated = "✅"
-            else:
-                checkValueAnimated = "❌"
-            print(f" {itemNumber} {checkName.capitalize()}: {checkValueAnimated}")
-            itemNumberMenu[itemNumber] = {"check": checkName, "value": checkValue,"category": name}
-            itemNumber += 1
-            if not checkValue:
-                blockers.append({'check': checkName, 'category': name})
-print(divider)
-header1 = f"Blockers"
-print(header1.center(len(header)))
-print(divider)
-if len(blockers) <= 0:
-    print("No blockers found 🥳")
+report = generateReport(data)
+for i in report:
+    print(i)
+numberedCheck = getNumberedChecks(data)
+updateDecision = input("Would you like to update anything in the checklist? (y/n)\n").lower()
+if updateDecision == "y":
+    updateNumbers = [int(x) for x in input("Please enter the number of the items you'd like to update seperated by a coma: \n").split(",")]
+    updatedData = updateChecklist(data, numberedCheck, updateNumbers)
+    print("Updating checklist...")
+    reportUpdated = generateReport(updatedData)
+    for i in reportUpdated:
+        print(i)
+    print("Saving checklist...")
+    saveChecklist(updatedData,filePath)
 else:
-    for blocker in blockers:
-        print(f"{blocker['check']} ({blocker['category']})")
-print(divider)
-updateNumbers = [int(x) for x in input("Please enter the number of the items you'd like to update seperated by a coma: \n").split(",")]
-for item in updateNumbers:
-    if item in itemNumberMenu:
-        menuItem = itemNumberMenu[item]
-        currentValue = menuItem['value']
-        newValue = not menuItem['value']
-        category = menuItem['category']
-        checkName = menuItem['check']
-        data[category][checkName] = newValue
-
-print("Updating checklist...")
-print(divider)
-print(header.center(len(header)))
-print(divider)
-for name, categoryData in data.items():
-    if name not in ["projectName", "owner"]:
-        totalChecks = len(categoryData)
-        completedChecks = sum(categoryData.values())
-        percentageCompleted = round((completedChecks/totalChecks) * 100,2)
-        print(f"{name}: {percentageCompleted}%")
-        for checkName in categoryData:
-            checkValue = categoryData[checkName]
-            if checkValue:
-                checkValueAnimated = "✅"
-            else:
-                checkValueAnimated = "❌"
-            print(f" {itemNumber} {checkName.capitalize()}: {checkValueAnimated}")
-            if not checkValue:
-                blockers.append({'check': checkName, 'category': name})
-print(divider)
-print(header1.center(len(header)))
-print(divider)
-if len(blockers) <= 0:
-    print("No blockers found 🥳")
-else:
-    for blocker in blockers:
-        print(f"{blocker['check']} ({blocker['category']})")
-print(divider)
-
-saveChecklist(data, filePath)
+    print("No changes made to file. Goodbye!")
 
